20020aba8daa266ab70f843dba4a1bc7ea5b933f
Amit P <aspper20@gmail.com>
2022-12-22 01:17:16 +0530

    feat: added header and media assets

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.tap-target').tapTarget();
+    $('.carousel').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+  });
